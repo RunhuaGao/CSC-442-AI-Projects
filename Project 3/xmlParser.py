b098e320d33bd81b844da513e54ed4b9d00b5cd0
Runhua Gao <43771629+RunhuaGao@users.noreply.github.com>
2018-11-10 00:04:05 -0500

    xmlParseFile with xml.dom.minidom
    Reply on Random variable, Table class and query data Structure
    query: {"node":{nodename:status},"evidence":{evidence name:evidence status},"probability:num"}

---
+++ Project 3/xmlParser.py
@@ -0,0 +1,91 @@
+file1 = "aima-alarm.xml"
+file2 = "aima-wet-grass.xml"
+file3 = "dog-problem.xml"
+va = "VARIABLE"
+name = "NAME"
+outcome = "OUTCOME"
+state = "FOR"
+give = "GIVEN"
+table = "TABLE"
+
+import xml.dom.minidom
+from RandomVariables import rv, Table
+
+
+# return a parser document root element
+def parser(file):
+    return xml.dom.minidom.parse(file).documentElement
+
+
+# return all the random variables in class rv
+def getVariables(root):
+    # parse variable tag
+    def parseVariableTag(tag):
+        global name
+        variablename = tag.getElementsByTagName(name)[0].firstChild.data
+        domain = []
+        for k in tag.getElementsByTagName(outcome):
+            domain.append(k.firstChild.data)
+        return variablename, rv(variablename, domain)
+
+    vas = {}
+    for tag in root.getElementsByTagName(va):
+        name, randomvariable = parseVariableTag(tag)
+        vas[name] = randomvariable
+    return vas
+
+
+def getDefinitions(root, rvs):
+    def parseDefinitionTag(tag):
+        statename = tag.getElementsByTagName(state)[0].firstChild.data
+        evidencerv = []
+        for k in tag.getElementsByTagName(give):
+            evidencerv.append(k.firstChild.data)
+        tableitems = parseTableTag(tag.getElementsByTagName(table)[0])
+        curtable = Table(rvs[statename], [rvs[n] for n in evidencerv], tableitems)
+        rvs[statename].addproba(curtable)
+
+    definitions = []
+    for i in root.getElementsByTagName("DEFINITION"):
+        definitions.append(parseDefinitionTag(i))
+    return definitions
+
+
+# parse the Table tag
+def parseTableTag(tag):
+    tableitems = []
+    for node in tag.childNodes:
+        tableitems.append(node.data)
+    return parseTable(tableitems)
+
+
+# parse the Table()
+def parseTable(tableitems):
+    result = []
+    for item in tableitems:
+        value = item.strip()
+        if value:
+            result.append(value)
+    return result
+
+
+# main function, build the node graph from xml file,return all nodes
+def parseFile(filename):
+    root = parser(filename)
+    rvs = getVariables(root)
+    getDefinitions(root, rvs)
+    return rvs
+
+
+# testFunction
+def test():
+    rvs = parseFile(file3)
+    return rvs
+
+
+for file in [file1, file2, file3]:
+    rvs = parseFile(file)
+    for r in rvs.values():
+        for k in r.table.query:
+            print(k)
+    print("%s has been parsed..\n"%file)
